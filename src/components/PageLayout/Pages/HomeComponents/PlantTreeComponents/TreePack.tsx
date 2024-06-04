8db8b0f21fdb1a6f3abb2cd045189cbbfc4903aa
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-04 09:22:02 +0100

    tree pack update

--- src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/TreePack.tsx
+++ src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/TreePack.tsx
@@ -3,7 +3,7 @@ import TreePackGroup from "./TreePackGroup"
 const TreePack = () => {
   return (
     <div className="max-sm:flex max-sm:items-center max-sm:justify-center max-sm:flex-col">
-        <p className=" text-[40px] max-sm:text-[20px] text-bgGreen md:ml-[168px] max-sm:text-center mt-40 font-medium">Choose your tree pack</p>
+        <p className=" text-[40px] max-sm:text-[28px] text-bgGreen md:ml-[168px] max-sm:text-center mt-40 font-medium">Choose your tree pack</p>
         <TreePackGroup/>
     </div>
   )
