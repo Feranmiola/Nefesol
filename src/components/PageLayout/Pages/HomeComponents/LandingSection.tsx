792ec73e4d1344339d2e3a5109ff113c25bc4046
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-04 07:04:56 +0100

    landing mobile support

--- src/components/PageLayout/Pages/HomeComponents/LandingSection.tsx
+++ src/components/PageLayout/Pages/HomeComponents/LandingSection.tsx
@@ -13,18 +13,17 @@ const LandingSection = () => {
     <div className="homeBackground ">
       <div className=" py-60 flex items-center justify-center flex-col space-y-10">
         <div className="flex items-center justify-center flex-col">
-          <h1 className="text-[150px] text-white">Every Tree</h1>
-          <h1 className="text-[150px] text-white">One Breath</h1>
+          <h1 className="text-[150px] max-sm:text-[80px] leading-tight tracking-tight text-white">Every Tree</h1>
+          <h1 className="text-[150px] max-sm:text-[80px] leading-tight tracking-tight text-white">One Breath</h1>
         </div>
         <div>
-          <p className="text-[24px] text-white takeAction">TAKE ACTION FOR A GREEN FUTURE</p>
+          <p className="text-[24px] max-sm:text-[10px] text-white takeAction">TAKE ACTION FOR A GREEN FUTURE</p>
         </div>
-        <div className="flex flex-row space-x-4">
+        <div className="flex md:flex-row max-sm:flex-col max-sm:space-y-4 smd:pace-x-4">
           <motion.a
-
           whileTap={{ scale: 0.9 }} 
           onClick={()=> handleNext('/co2-calculator')}
-          className="w-[280px] h-[64px] bg-[#25B567] hover:bg-[#1a8249]  transition ease-in-out flex flex-row space-x-2 items-center justify-center rounded-[56px] cursor-pointer"
+          className="w-[280px] h-[64px] max-sm:w-[240px] max-sm:h-[62px] bg-[#25B567] hover:bg-[#1a8249]  transition ease-in-out flex flex-row space-x-2 items-center justify-center rounded-[56px] cursor-pointer"
           >
             <p className="text-white text-[16px] font-medium">Breathe Now</p>
             <img
@@ -36,7 +35,7 @@ const LandingSection = () => {
           <motion.a
             whileTap={{ scale: 0.9 }} 
             onClick={()=> handleNext('/ourservices')}
-            className="w-[280px] h-[64px] bg-transparent hover:bg-slate-900 hover:bg-opacity-30 hover:ring-bgGreen hover:ring-2 ease-in-out ring-[1px] ring-white flex flex-row space-x-2 items-center justify-center rounded-[56px] cursor-pointer"
+            className="w-[280px] h-[64px] max-sm:w-[240px] max-sm:h-[62px] bg-transparent hover:bg-slate-900 hover:bg-opacity-30 hover:ring-bgGreen hover:ring-2 ease-in-out ring-[1px] ring-white flex flex-row space-x-2 items-center justify-center rounded-[56px] cursor-pointer"
             >
               <p className="text-white text-[16px] font-medium">Get Information</p>
           </motion.a>
