f11de626c3067475da92266b6dafe4b100849a5b
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-04 07:25:53 +0100

    brethe now mobile support

--- src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/BretheNow.tsx
+++ src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/BretheNow.tsx
@@ -14,30 +14,30 @@ const BretheNow = () => {
       }
 
   return (
-    <div className="flex flex-col">
-        <div className="bg-white rounded-[24px] w-[455px] shadow-md ">
+    <div className="flex flex-col max-sm:mt-10 max-sm:items-center max-sm:justify-center">
+        <div className="bg-white rounded-[24px] w-[455px] max-sm:w-[355px] shadow-md ">
             <div className="flex items-center flex-col space-y-9 justify-center bg-[#F8F9F8] p-5">
                 <p className="text-[24px] text-center text-bgGreen font-bold w-[360px]">How many trees do you want to plant in the forest?</p>
                 <div className="flex flex-row space-x-3">
                     <motion.div
                     whileHover={{ scale: 1.03 }}
                     whileTap={{ scale: 0.9 }} 
-                     className="bg-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 px-5">
-                        <p className="text-white ">Slider</p>
+                     className="bg-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 md:px-5 max-sm:px-3">
+                        <p className="text-white max-sm:text-[10px] ">Slider</p>
                     </motion.div>
 
                     <motion.div
                     whileHover={{ scale: 1.03 }}
                     whileTap={{ scale: 0.9 }} 
-                     className="bg-transparent ring-[1px] ring-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 px-5">
-                        <p className="text-bgGreen ">CO2 Quantity</p>
+                     className="bg-transparent ring-[1px] ring-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 md:px-5 max-sm:px-3">
+                        <p className="text-bgGreen max-sm:text-[10px]">CO2 Quantity</p>
                     </motion.div>
 
                     <motion.div
                     whileHover={{ scale: 1.03 }}
                     whileTap={{ scale: 0.9 }} 
-                     className="bg-transparent ring-[1px] ring-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 px-5">
-                        <p className="text-bgGreen ">Examples</p>
+                     className="bg-transparent ring-[1px] ring-bgGreen rounded-full flex items-center justify-center cursor-pointer py-1 md:px-5 max-sm:px-3">
+                        <p className="text-bgGreen max-sm:text-[10px] ">Examples</p>
                     </motion.div>
                 </div>
                 <div className="w-[100%] flex items-center justify-center">
@@ -48,7 +48,7 @@ const BretheNow = () => {
                     />
                 </div>
             </div>
-            <div className="flex flex-row py-6 space-x-10 items-center justify-center">
+            <div className="flex flex-row max-sm:flex-col py-6 md:space-x-10 max-md:space-y-4 items-center justify-center">
                 <img
                 src="./assets/treeGroup.svg"
                 />
@@ -64,7 +64,7 @@ const BretheNow = () => {
                 </div>
 
             </div>
-            <div className="flex items-center justify-center flex-row space-x-3 pt-5 pb-5">
+            <div className="flex items-center justify-center flex-row max-sm:flex-col md:space-x-3 max-sm:space-y-3 pt-5 md:pb-5">
                 <div className="bg-[#0A5B361A] flex flex-row items-center justify-center px- h-[56px] w-[186px]">
                     <img
                     src="./assets/field.svg"
@@ -86,7 +86,7 @@ const BretheNow = () => {
             <div className="py-10">
                 <Separator className="w-full fill-[#0A4519]"/>
             </div>
-            <div className="flex items-center justify-between px-8 pb-10 flex-row">
+            <div className="flex items-center justify-between px-8 pb-10 flex-row max-sm:flex-col max-sm:space-y-3">
                 <div className="flex flex-col">
                     <p className="text-bgGreen text-[32px]">$220.29</p>
                     <p className="text-[12px] text-bgGreen">Unit Price 1.20 $/m2</p>
@@ -101,7 +101,7 @@ const BretheNow = () => {
             </div>
         </div>
         <div>
-            <p className="text-[10px] text-bgGreen w-[437px] pt-5">The figures are average values based on the binding potential of the trees. According to the latest scientific findings, the average lifespan of trees is between 30 and 50 years and can bind up to 150 kg of CO2 per year. Calculations are based on average data and may vary according to tree type and age.</p>
+            <p className="text-[10px] text-bgGreen md:w-[437px] max-sm:w-[300px]  pt-5">The figures are average values based on the binding potential of the trees. According to the latest scientific findings, the average lifespan of trees is between 30 and 50 years and can bind up to 150 kg of CO2 per year. Calculations are based on average data and may vary according to tree type and age.</p>
         </div>
         
     </div>
