46b145ecadcc42184fd26845966b1bf9a7cfccce
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-08 22:07:23 +0100

    "Added content to CancellationRefundPolicy component, including text, styles, and layout."

--- src/components/PageLayout/Pages/Legal/CancellationRefundPolicy.tsx
+++ src/components/PageLayout/Pages/Legal/CancellationRefundPolicy.tsx
@@ -1,7 +1,196 @@
 
 const CancellationRefundPolicy = () => {
   return (
-    <div>CancellationRefundPolicy</div>
+    <div className="py-10 md:py-20 mt-5 md:mt-10 w-full">
+    <div className="flex flex-col space-y-5 items-center justify-center">
+      <p className="text-linkGreen font-semibold text-[16px] md:text-[18px] text-center">
+        İptal İade Politikası
+      </p>
+      <p className='text-bgGreen text-[40px] max-sm:text-[20px] text-center font-medium w-[1145px] max-sm:w-[300px]'>TÜKETİCİ HAKLARI – CAYMA – İPTAL İADE KOŞULLARI</p>
+
+      <div className="flex flex-col space-y-2 md:space-y-5 px-4 md:px-0 max-w-[70rem]">
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">GENEL:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Kullanmakta olduğunuz web sitesi üzerinden elektronik ortamda sipariş verdiğiniz takdirde, size sunulan ön bilgilendirme formunu ve mesafeli satış sözleşmesini kabul etmiş sayılırsınız.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Alıcılar, satın aldıkları ürünün satış ve teslimi ile ilgili olarak 6502 sayılı Tüketicinin Korunması Hakkında Kanun ve Mesafeli Sözleşmeler Yönetmeliği
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                (RG:27.11.2014/29188) hükümleri ile yürürlükteki diğer yasalara tabidir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Ürün sevkiyat masrafı olan kargo ücretleri alıcılar tarafından ödenecektir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Satın alınan her bir ürün, 30 günlük yasal süreyi aşmamak kaydı ile alıcının gösterdiği adresteki kişi ve/veya kuruluşa teslim edilir. Bu süre içinde ürün teslim edilmez ise, Alıcılar sözleşmeyi sona erdirebilir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Satın alınan ürün, eksiksiz ve siparişte belirtilen niteliklere uygun ve varsa garanti belgesi, kullanım klavuzu gibi belgelerle teslim edilmek zorundadır.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Satın alınan ürünün satılmasının imkansızlaşması durumunda, satıcı bu durumu öğrendiğinden itibaren 3 gün içinde yazılı olarak alıcıya bu durumu bildirmek
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                zorundadır. 14 gün içinde de toplam bedel Alıcı’ya iade edilmek zorundadır.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">SATIN ALINAN ÜRÜN BEDELİ ÖDENMEZ İSE:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Alıcı, satın aldığı ürün veya hizmetin bedelini ödemez veya banka kayıtlarında iptal ederse, Satıcının ürünü veya hizmeti teslim yükümlülüğü sona erer.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">KREDİ KARTININ YETKİSİZ KULLANIMI İLE YAPILAN ALIŞVERİŞLER:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Ürün teslim edildikten sonra, alıcının ödeme yaptığı kredi kartının yetkisiz kişiler tarafından haksız olarak kullanıldığı tespit edilirse ve satılan ürün veya hizmet bedeli ilgili banka veya finans kuruluşu tarafından Satıcı'ya ödenmez ise, Alıcı, sözleşme konusu ürünü 3 gün içerisinde nakliye gideri SATICI’ya ait olacak şekilde SATICI’ya iade etmek zorundadır.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">ÖNGÖRÜLEMEYEN SEBEPLERLE ÜRÜN SÜRESİNDE TESLİM EDİLEMEZ İSE:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Satıcı’nın öngöremeyeceği mücbir sebepler oluşursa ve ürün süresinde teslim edilemez ise, durum Alıcı’ya bildirilir. Alıcı, siparişin iptalini, ürünün benzeri ile değiştirilmesini veya engel ortadan kalkana dek teslimatın ertelenmesini talep edebilir. Alıcı siparişi iptal ederse; ödemeyi nakit ile yapmış ise iptalinden itibaren 14 gün içinde kendisine nakden bu ücret ödenir. Alıcı, ödemeyi kredi kartı ile yapmış ise ve iptal ederse, bu iptalden itibaren yine 14 gün içinde ürün bedeli bankaya iade edilir, ancak bankanın alıcının hesabına 2-3 hafta içerisinde aktarması olasıdır.
+                </p>
+            </div>
+        </div>
+        
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">ALICININ ÜRÜNÜ KONTROL ETME YÜKÜMLÜLÜĞÜ:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Alıcı, sözleşme konusu mal/hizmeti teslim almadan önce muayene edecek; ezik, kırık, ambalajı yırtılmış vb. hasarlı ve ayıplı mal/hizmeti kargo şirketinden teslim almayacaktır. Teslim alınan mal/hizmetin hasarsız ve sağlam olduğu kabul edilecektir. ALICI , Teslimden sonra mal/hizmeti özenle korunmak zorundadır. Cayma hakkı kullanılacaksa mal/hizmet kullanılmamalıdır. Ürünle birlikte Fatura da iade edilmelidir.
+
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">CAYMA HAKKI:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ALICI; satın aldığı ürünün kendisine veya gösterdiği adresteki kişi/kuruluşa teslim tarihinden itibaren 14 (on dört) gün içerisinde, SATICI’ya aşağıdaki iletişim bilgileri üzerinden bildirmek şartıyla hiçbir hukuki ve cezai sorumluluk üstlenmeksizin ve hiçbir gerekçe göstermeksizin malı reddederek sözleşmeden cayma hakkını kullanabilir.
+
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">SATICININ CAYMA HAKKI BİLDİRİMİ YAPILACAK İLETİŞİM BİLGİLERİ:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ŞİRKET
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ADI/UNVANI:ERMA GROUP TURİZM SANAYİ TİCARET ANONİM ŞİRKETİ
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ADRES:ÇAMTEPE MAH. MAHMUT TEVFİK ATAY BULV. GAZİANTEP TEKNOPARK NO:4-A İÇ KAPO NO:1 ŞEHİTKAMİL GAZİANTEP
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                EPOSTA: info@nefesol.com
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                TEL:0850 309 8804
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">CAYMA HAKKININ SÜRESİ:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Alıcı, satın aldığı eğer bir hizmet ise, bu 14 günlük süre sözleşmenin imzalandığı tarihten itibaren başlar. Cayma hakkı süresi sona ermeden önce, tüketicinin onayı ile
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                hizmetin ifasına başlanan hizmet sözleşmelerinde cayma hakkı kullanılamaz.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Cayma hakkının kullanımından kaynaklanan masraflar SATICI’ ya aittir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Cayma hakkının kullanılması için 14 (ondört) günlük süre içinde SATICI' ya iadeli taahhütlü posta, faks veya eposta ile yazılı bildirimde bulunulması ve ürünün işbu sözleşmede düzenlenen "Cayma Hakkı Kullanılamayacak Ürünler" hükümleri çerçevesinde kullanılmamış olması şarttır.
+                </p>
+                
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">CAYMA HAKKININ KULLANIMI:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                3. kişiye veya ALICI’ ya teslim edilen ürünün faturası, (İade edilmek istenen ürünün faturası kurumsal ise, iade ederken kurumun düzenlemiş olduğu iade faturası ile birlikte gönderilmesi gerekmektedir. Faturası kurumlar adına düzenlenen sipariş iadeleri İADE FATURASI kesilmediği takdirde tamamlanamayacaktır.) İade formu, İade edilecek ürünlerin kutusu, ambalajı, varsa standart aksesuarları ile birlikte eksiksiz ve hasarsız olarak teslim edilmesi gerekmektedir.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">İADE KOŞULLARI:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                SATICI, cayma bildiriminin kendisine ulaşmasından itibaren en geç 10 günlük süre içerisinde toplam bedeli ve ALICI’yı borç altına sokan belgeleri ALICI’ ya iade etmek ve 20 günlük süre içerisinde malı iade almakla yükümlüdür.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ALICI’ nın kusurundan kaynaklanan bir nedenle malın değerinde bir azalma olursa veya iade imkânsızlaşırsa ALICI kusuru oranında SATICI’ nın zararlarını tazmin etmekle yükümlüdür. Ancak cayma hakkı süresi içinde malın veya ürünün usulüne uygun kullanılması sebebiyle meydana gelen değişiklik ve bozulmalardan ALICI sorumlu değildir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Cayma hakkının kullanılması nedeniyle SATICI tarafından düzenlenen kampanya limit tutarının altına düşülmesi halinde kampanya kapsamında faydalanılan indirim miktarı iptal edilir.
+                </p>
+                
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">CAYMA HAKKI KULLANILAMAYACAK ÜRÜNLER:</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ALICI’nın isteği veya açıkça kişisel ihtiyaçları doğrultusunda hazırlanan ve geri gönderilmeye müsait olmayan, iç giyim alt parçaları, mayo ve bikini altları, makyaj malzemeleri, tek kullanımlık ürünler, çabuk bozulma tehlikesi olan veya son kullanma tarihi geçme ihtimali olan mallar, ALICI’ya teslim edilmesinin ardından ALICI tarafından ambalajı açıldığı takdirde iade edilmesi sağlık ve hijyen açısından uygun olmayan ürünler, teslim edildikten sonra başka ürünlerle karışan ve doğası gereği ayrıştırılması mümkün olmayan ürünler, Abonelik sözleşmesi kapsamında sağlananlar dışında, gazete ve dergi gibi süreli yayınlara ilişkin mallar, Elektronik ortamda anında ifa edilen hizmetler veya tüketiciye anında teslim edilen gayrimaddi mallar, ile ses veya görüntü kayıtlarının, kitap, dijital içerik, yazılım programlarının, veri kaydedebilme ve veri depolama cihazlarının, bilgisayar sarf malzemelerinin, ambalajının ALICI tarafından açılmış olması halinde iadesi Yönetmelik gereği mümkün değildir. Kişiye özel dikimi yapılan ağaçlar, bitkiler, firma veya kişiye özel yapılmış yazılımlar, Ayrıca Cayma hakkı süresi sona ermeden önce, tüketicinin onayı ile ifasına başlanan hizmetlere ilişkin cayma hakkının kullanılması da Yönetmelik gereği mümkün değildir.
+                </p>
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Kozmetik ve kişisel bakım ürünleri, iç giyim ürünleri, mayo, bikini, kitap, kopyalanabilir yazılım ve programlar, DVD, VCD, CD ve kasetler ile kırtasiye sarf malzemeleri (toner, kartuş, şerit vb.) iade edilebilmesi için ambalajlarının açılmamış, denenmemiş, bozulmamış ve kullanılmamış olmaları gerekir.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">TEMERRÜT HALİ VE HUKUKİ SONUÇLARI</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                ALICI, ödeme işlemlerini kredi kartı ile yaptığı durumda temerrüde düştüğü takdirde, kart sahibi banka ile arasındaki kredi kartı sözleşmesi çerçevesinde faiz ödeyeceğini ve bankaya karşı sorumlu olacağını kabul, beyan ve taahhüt eder. Bu durumda ilgili banka hukuki yollara başvurabilir; doğacak masrafları ve vekâlet ücretini ALICI’dan talep edebilir ve her koşulda ALICI’nın borcundan dolayı temerrüde düşmesi halinde, ALICI, borcun gecikmeli ifasından dolayı SATICI’nın uğradığı zarar ve ziyanını ödeyeceğini kabul eder.
+                </p>
+            </div>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+            <p className="text-[14px] md:text-[16px] text-bgGreen font-semibold">ÖDEME VE TESLİMAT</p>
+            <div className="flex  flex-col space-y-1">
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Banka Havalesi veya EFT (Elektronik Fon Transferi) yaparak,Iban: TR79 0001 2009 3710 0010 2618 55 Halk bankası bankası (TL) hesabımıza yapabilirsiniz.
+                </p>
+
+                <p className="text-[14px] md:text-[16px] text-bgGreen">
+                Sitemiz üzerinden kredi kartlarınız ile, Her türlü kredi kartınıza online tek ödeme ya da online taksit imkânlarından yararlanabilirsiniz. Online ödemelerinizde siparişiniz sonunda kredi kartınızdan tutar çekim işlemi gerçekleşecektir.
+                </p>
+
+            </div>
+        </div>
+
+
+
+      </div>
+    </div>
+  </div>
   )
 }
 
