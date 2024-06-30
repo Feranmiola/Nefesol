6ef50e4f37d9556afd0c7158239832014456780f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-30 17:47:44 +0100

    "Added i18n translation to 'Choose your tree pack' text in TreePack component"

--- src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/TreePack.tsx
+++ src/components/PageLayout/Pages/HomeComponents/PlantTreeComponents/TreePack.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from "react-i18next"
 import TreePackGroup from "./TreePackGroup"
 
 const TreePack = () => {
+  const {t} = useTranslation();
   return (
     <div className="max-sm:flex max-sm:items-center max-sm:justify-center max-sm:flex-col">
-        <p className=" text-[40px] max-sm:text-[28px] text-bgGreen md:ml-[168px] max-sm:text-center mt-40 font-medium">Choose your tree pack</p>
+        <p className=" text-[40px] max-sm:text-[28px] text-bgGreen md:ml-[168px] max-sm:text-center mt-40 font-medium">{t('chooseTreePack')}</p>
         <TreePackGroup/>
     </div>
   )
