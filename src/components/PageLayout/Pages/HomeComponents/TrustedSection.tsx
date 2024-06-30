6c0ff1c2207906eb57a461bbdea599d405817a7f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-30 18:07:54 +0100

    "Added react-i18next translation to TrustedSection component"

--- src/components/PageLayout/Pages/HomeComponents/TrustedSection.tsx
+++ src/components/PageLayout/Pages/HomeComponents/TrustedSection.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "react-i18next"
 import TrestedGroup from "./TrestedGroup"
 const TrustedSection = () => {
+  const {t} = useTranslation();
   return (
     <div className="md:py-20 max-sm:flex max-sm:items-center max-sm:flex-col max-sm:justify-center">
-        <p className="text-[40px] max-sm:text-[30px] text-bgGreen md:ml-[168px]">We are trusted by</p>
+        <p className="text-[40px] max-sm:text-[30px] text-bgGreen md:ml-[168px]">{t('trustedBy')}</p>
         <TrestedGroup/>
     </div>
   )
