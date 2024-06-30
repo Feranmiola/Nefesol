5eeb5b2af4c1ed39a5215134d4d66e2e6ced4bcc
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-06-30 23:08:07 +0100

    "Translated Payment page to support multiple languages using react-i18next"

--- src/components/PageLayout/Pages/PlantTreesPages/Payment.tsx
+++ src/components/PageLayout/Pages/PlantTreesPages/Payment.tsx
@@ -10,11 +10,12 @@ import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox"
 import { useEffect, useState } from "react"
 import BeatLoader from "react-spinners/BeatLoader"
+import { useTranslation } from "react-i18next"
 
 
 const Payment = () => {
     
-    
+    const {t} = useTranslation();
 
     const [loading, setLoading] = useState(true);
     const imageUrls = ['./assets/prev.svg', './assets/treeGroup.svg', './assets/greenDot.svg', './assets/minus.svg', './assets/plusIcon.svg']; // Replace with your image URLs
@@ -47,31 +48,31 @@ const Payment = () => {
         <div className=" max-w-md lg:max-w-2xl lg:flex lg:items-center lg:justify-between bg-white rounded-[24px]">
             <div className="flex flex-col">
                 <div className="bg-[#F8F9F8] h-[80px] px-5 md:px-10 rounded-t-[24px] flex flex-row justify-between items-center">
-                    <p className="text-bgGreen text-[20px] md:text-[24px] font-bold">Plant Trees</p>
+                    <p className="text-bgGreen text-[20px] md:text-[24px] font-bold">{t('plantTrees')}</p>
                     <motion.div
                         whileHover={{ scale: 1.06 }}
                         whileTap={{ scale: 0.9 }} 
                         className="flex flex-row space-x-2"
                     >
                         <img src="./assets/prev.svg" />
-                        <a href="/co2-calculator" className="text-[14px] md:text-[16px] text-linkGreen font-semibold">Return to calculator</a>
+                        <a href="/co2-calculator" className="text-[14px] md:text-[16px] text-linkGreen font-semibold">{t('returnToCalculator')}</a>
                     </motion.div>
                 </div>
                 
                 <div className="flex items-center flex-col justify-center py-5">
-                    <p className="text-bgGreen text-[16px]">You’re about to plant</p>
+                    <p className="text-bgGreen text-[16px]">{t('youAreAboutToPlant')}</p>
                     <div className="flex flex-col space-y-1">
                         <div className="flex flex-row py-6 space-x-5 items-center justify-center">
                             <img src="./assets/treeGroup.svg" />
                             <div className="flex flex-row max-sm:flex-col items-center max-sm:justify-center">
                                 <p className="text-bgGreen text-[32px]"><span className="text-[48px]">12,000</span></p>
-                                <p className="text-bgGreen text-[32px]">Trees</p>
+                                <p className="text-bgGreen text-[32px]">{t('trees')}</p>
                             </div>
                         </div>
                         <div className="flex flex-row space-x-2 items-center justify-center opacity-60">
-                            <p className="text-bgGreen text-[12px]">Based on your CO2 use calculation</p>
+                            <p className="text-bgGreen text-[12px]">{t('basedOnYourCO2')}</p>
                             <img src="./assets/greenDot.svg" />
-                            <p className="text-bgGreen text-[12px]">Unit Price: 1.20 $/tree</p>
+                            <p className="text-bgGreen text-[12px]">{t('unitPrice2')}</p>
                         </div>
                     </div>
                     <div className="w-[180px] md:w-[234px] h-[52px] rounded-[8px] mt-2 flex space-x-2 items-center justify-center bg-[#E1EAE5]">
@@ -85,15 +86,15 @@ const Payment = () => {
 
                 <div className="flex flex-col space-y-5 items-center justify-center px-5">
                     <div className="flex flex-col space-y-3 w-full">
-                        <p className="text-bgGreen text-[16px]">Email</p>
-                        <Input placeholder="Enter Email" type="email" className="shad-input-plant-width"/>
+                        <p className="text-bgGreen text-[16px]">{t('email')}</p>
+                        <Input placeholder={t('enterEmail')} type="email" className="shad-input-plant-width"/>
                     </div>
                     <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Preferred Planting Location</p>
+                            <p className="text-bgGreen text-[16px]">{t('preferredPlantingLocation')}</p>
                             <Select>
                                 <SelectTrigger className="shad-select-plant data-[placeholder]:text-[#0a45194f]">
-                                    <SelectValue placeholder="Select" />
+                                    <SelectValue placeholder={t('select')} />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectItem value="light">Light</SelectItem>
@@ -103,10 +104,10 @@ const Payment = () => {
                             </Select>
                         </div>
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Payment Method</p>
+                            <p className="text-bgGreen text-[16px]">{t('paymentMethod')}</p>
                             <Select>
                                 <SelectTrigger className="shad-select-plant data-[placeholder]:text-[#0a45194f]">
-                                    <SelectValue placeholder="Select" />
+                                    <SelectValue placeholder={t('select')} />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectItem value="light">Light</SelectItem>
@@ -119,45 +120,45 @@ const Payment = () => {
 
                     <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">First Name</p>
-                            <Input placeholder="Enter First Name" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('firstName')}</p>
+                            <Input placeholder={t('enterFirstName')} className="shad-input-plant"/>
                         </div>
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Last Name</p>
-                            <Input placeholder="Enter Last Name" className="shad-input-plant"/>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
-                        <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">City</p>
-                            <Input placeholder="Enter City" className="shad-input-plant"/>
-                        </div>
-                        <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Town</p>
-                            <Input placeholder="Enter Town" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('lastName')}</p>
+                            <Input placeholder={t('enterLastName')} className="shad-input-plant"/>
                         </div>
                     </div>
 
                     <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Street</p>
-                            <Input placeholder="Enter Street" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('city')}</p>
+                            <Input placeholder={t('enterCity')} className="shad-input-plant"/>
                         </div>
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Post Code</p>
-                            <Input placeholder="Enter Post Code" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('town')}</p>
+                            <Input placeholder={t('enterTown')} className="shad-input-plant"/>
                         </div>
                     </div>
 
                     <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Tax Number</p>
-                            <Input placeholder="Enter Tax Number" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('street')}</p>
+                            <Input placeholder={t('enterStreet')} className="shad-input-plant"/>
                         </div>
                         <div className="flex flex-col space-y-3 w-full">
-                            <p className="text-bgGreen text-[16px]">Tax Administrator</p>
-                            <Input placeholder="Enter Tax Administrator" className="shad-input-plant"/>
+                            <p className="text-bgGreen text-[16px]">{t('postCode')}</p>
+                            <Input placeholder={t('enterPostCode')} className="shad-input-plant"/>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-5 space-y-5 md:space-y-0 w-full">
+                        <div className="flex flex-col space-y-3 w-full">
+                            <p className="text-bgGreen text-[16px]">{t('taxNumber')}</p>
+                            <Input placeholder={t('enterTaxNumber')} className="shad-input-plant"/>
+                        </div>
+                        <div className="flex flex-col space-y-3 w-full">
+                            <p className="text-bgGreen text-[16px]">{t('taxAdministrator')}</p>
+                            <Input placeholder={t('enterTaxAdministrator')} className="shad-input-plant"/>
                         </div>
                     </div>
                 </div>
@@ -168,7 +169,7 @@ const Payment = () => {
                             htmlFor="terms"
                             className="text-bgGreen text-[16px]"
                         >
-                            I have read and agree to the <a href="/plant-trees-payment" target="blank" className="text-linkGreen">terms and conditions</a>
+                            {t('iHaveReadAndAgree')} <a href="/plant-trees-payment" target="blank" className="text-linkGreen">{t('termsAndConditions')}</a>
                         </label>
                     </div>
 
@@ -178,7 +179,7 @@ const Payment = () => {
                             href="/plant-trees-verifyemail"
                             className="w-[85%] h-[56px] bg-[#25B567] hover:bg-[#1a8249] transition ease-in-out flex flex-row space-x-2 items-center justify-center rounded-[56px] cursor-pointer"
                         >
-                            <p className="text-white text-[16px] font-medium">Next</p>
+                            <p className="text-white text-[16px] font-medium">{t('next')}</p>
                         </motion.a>
                     </div>
                 </div>
